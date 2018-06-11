73fe4da4cccaea473b610064e13527ef8f2d3028
Shamworldtime <40053345+Shamworldtime@users.noreply.github.com>
2018-06-11 01:36:48 -0700

    Add files via upload

--- Newboi.py
+++ Newboi.py
@@ -12,8 +12,39 @@ import scipy.sparse.linalg as la
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from pynput.mouse import Button, Controller, Listener
 
 
+#start and finishing time
+t_start = 0
+t_final = 1000
+
+t = t_start
+t_click = np.inf
+x_click = 0
+y_click = 0
+
+#time-step
+dt = 1
+
+def on_press(event):
+    global t
+    global t_click
+    global x_click
+    global y_click
+    global dt
+    t_click = t + 2*dt
+    x_click = event.xdata
+    y_click = event.ydata
+
+
+#def press(x, y, button, pressed):
+#    global t
+
+
+#with Listener(on_click=press) as listener:
+#    listener.join()
+    
 #Setting up domain of problem
 
 xL = -2
@@ -29,33 +60,35 @@ DiffCo = 0.0015
 #Boundary Cofficients
 def Tboundary(t):
 
-    T_bc = min([20+(4/3)*t, 100])
+    #T_bc = min([20+(4/3)*t, 100])
+    T_bc = 20
     return(T_bc)
 
-#source term
-def source(x,y,t):
-    f = 0
-    return(f)
 
 #initial temp
 T_initial = 20
 
-#start and finishing time
-t_start = 0
-t_final = 1000
-
 
 #setting up co-ordinates
 Nx = 80
-Ny = 100
+Ny = 80
 x = np.linspace(xL, xR, Nx)
-y = np.linspace(yB, yT, Ny)
+y = np.linspace(yB, yT, Ny)[::-1]
 dx = (xR-xL)/(Nx-1)
 dy = (yT-yB)/(Ny-1)
 
-#time-step
 
-dt = 5
+
+#source term
+def source(x,y,t):
+    global t_click
+    global x_click
+    global y_click
+    f = 0
+    if(abs(x-x_click)<= 0.1 and abs(y-y_click) <= 0.1 and t == t_click):
+        #print(y)
+        f = 50
+    return(f)
 
 T_old = np.full((Nx*Ny,1), T_initial)
 T_new = np.full((Nx*Ny,1), T_initial)
@@ -100,10 +133,13 @@ for i in range(Nx):
 
 A = csc_matrix((values, (nonzero_row, nonzero_col)), shape = (Nx*Ny,Nx*Ny))
 fig, ax = plt.subplots(1,1)
+
+cid = fig.canvas.mpl_connect('button_press_event', on_press)
+
 while t < t_final:
     for i in range(1,Nx-1):
         for j in range(1, Ny-1):
-            RHS[point(i,j)] = T_old[point(i,j)] + dt*source(x[i],y[i],t+dt)
+            RHS[point(i,j)] = T_old[point(i,j)] + dt*source(x[i],y[j],t+dt)
 
             
     for i in range(Nx):
@@ -115,24 +151,27 @@ while t < t_final:
 
     T_new = la.spsolve(A, RHS)
 
-    T_matrix = np.full((Nx,Ny), 0)
+    T_matrix = np.full((Nx,Ny), 0.)
     X,Y = np.meshgrid(x,y)
     for i in range(Nx):
         for j in range(Ny):
             T_matrix[i][j] = T_old[point(i,j)]
     T_mat = np.transpose(T_matrix)
-
-    graph = ax.imshow(T_mat, cmap='jet')
+    graph = plt.imshow(T_mat, cmap='jet', extent = [xL, xR, yB, yT])
     if t == t_start:
         bar = fig.colorbar(graph, ax=ax)
     else:
         #bar = plt.colorbar(graph)
-        graph.autoscale()
-        bar.update_bruteforce(graph)
-    plt.pause(0.001)
-    T_old = T_new
+        if(T_mat.max()-T_mat.min() > 5):
+            graph.autoscale()
+            bar.update_bruteforce(graph)
+        else:
+            plt.clim(0,100)
+    plt.pause(0.0001)
+    T_old = T_new  
     t = t+dt
-
+    
+    
 plt.show()
     
 
